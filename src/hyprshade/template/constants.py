9a048c8c0711273753ecbad590a92aeafd7e10ec
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-04 10:10:19 -0500

    feat!: nuke

--- src/hyprshade/template/constants.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from typing import Final
-
-TEMPLATE_EXTENSIONS: Final = {"mustache"}
--- tests/helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .freeze_time import freeze_time
-from .systemd_unit_parser import SystemdUnitParser
-
-__all__ = ["freeze_time", "SystemdUnitParser"]
