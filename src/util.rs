08e346648ba942cf212437ad1b740805b74ff38a
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-20 23:35:11 -0500

    feat(util): `PathExt::extension()`

--- src/util.rs
+++ src/util.rs
@@ -2,15 +2,46 @@ use std::ffi::OsStr;
 
 pub trait PathExt {
     fn file_prefix(&self) -> Option<&OsStr>;
+    fn extension(&self) -> Option<&OsStr>;
 }
 
 impl PathExt for std::path::Path {
-    /// https://github.com/rust-lang/rust/issues/86319
     #[must_use]
     fn file_prefix(&self) -> Option<&OsStr> {
         self.file_name()
             .map(split_file_at_dot)
             .map(|(before, _after)| before)
+    }
+    #[must_use]
+    fn extension(&self) -> Option<&OsStr> {
+        self.file_name()
+            .map(rsplit_file_at_dot)
+            .and_then(|(before, after)| before.and(after))
+    }
+}
+
+// basic workhorse for splitting stem and extension
+fn rsplit_file_at_dot(file: &OsStr) -> (Option<&OsStr>, Option<&OsStr>) {
+    if file.as_encoded_bytes() == b".." {
+        return (Some(file), None);
+    }
+
+    // The unsafety here stems from converting between &OsStr and &[u8]
+    // and back. This is safe to do because (1) we only look at ASCII
+    // contents of the encoding and (2) new &OsStr values are produced
+    // only from ASCII-bounded slices of existing &OsStr values.
+    let mut iter = file.as_encoded_bytes().rsplitn(2, |b| *b == b'.');
+    let after = iter.next();
+    let before = iter.next();
+    if before == Some(b"") {
+        (Some(file), None)
+    } else {
+        unsafe {
+            (
+                before.map(|s| OsStr::from_encoded_bytes_unchecked(s)),
+                after.map(|s| OsStr::from_encoded_bytes_unchecked(s)),
+            )
+        }
     }
 }
 
