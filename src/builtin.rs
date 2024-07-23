5ce5707262d205f38615c20302b7f9bf186fce00
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-22 19:42:04 -0500

    feat(builtin): add metadata to all builtin shaders

--- src/builtin.rs
+++ src/builtin.rs
@@ -4,27 +4,124 @@ use phf::phf_map;
 pub struct BuiltinShader {
     pub contents: &'static str,
     pub is_template: bool,
+    pub metadata: Metadata,
+}
+
+#[derive(Debug)]
+pub struct Metadata {
+    pub full_name: &'static str,
+    pub description: &'static str,
+    pub variables: phf::Map<&'static str, Variable>,
+}
+
+#[derive(Debug)]
+pub enum Variable {
+    Float { default: f32, min: f32, max: f32 },
+    Enum(&'static [&'static str]),
+    Dict(phf::Map<&'static str, Variable>),
 }
 
 pub const BUILTIN_SHADERS: phf::Map<&'static str, BuiltinShader> = phf_map! {
     "blue-light-filter" => BuiltinShader {
         contents: include_str!("shaders/blue-light-filter.glsl.mustache"),
         is_template: true,
+        metadata: Metadata {
+            full_name: "Blue Light Filter",
+            description: "Use warmer colors to make the display easier on your eyes.",
+            variables: phf_map! {
+                "temperature" => Variable::Float {
+                    default: 2600.0,
+                    min: 1000.0,
+                    max: 40000.0,
+                },
+                "strength" => Variable::Float {
+                    default: 1.0,
+                    min: 0.0,
+                    max: 1.0,
+                },
+            },
+        },
     },
     "color-filter" => BuiltinShader {
         contents: include_str!("shaders/color-filter.glsl.mustache"),
         is_template: true,
+        metadata: Metadata {
+            full_name: "Color Filter",
+            description: "\
+                Adjust colors for color vision deficiencies.\n\
+                Supports protanopia (red-green), deuteranopia (green-red), and tritanopia (blue-yellow).\
+            ",
+            variables: phf_map! {
+                "type" => Variable::Enum(&[
+                    "protanopia",
+                    "protan",
+                    "redgreen",
+                    "deuteranopia",
+                    "deutan",
+                    "greenred",
+                    "tritanopia",
+                    "tritan",
+                    "blueyellow",
+                ]),
+                "strength" => Variable::Float {
+                    default: 0.2,
+                    min: 0.0,
+                    max: 1.0,
+                },
+            },
+        },
     },
     "grayscale" => BuiltinShader {
         contents: include_str!("shaders/grayscale.glsl.mustache"),
         is_template: true,
+        metadata: Metadata {
+            full_name: "Grayscale",
+            description: "Use grayscale filter",
+            variables: phf_map! {
+                "type" => Variable::Enum(&["luminosity", "lightness", "average"]),
+                "luminosity_type" => Variable::Enum(&["pal", "hdtv", "hdr"]),
+            },
+        },
     },
     "invert-colors" => BuiltinShader {
         contents: include_str!("shaders/invert-colors.glsl"),
         is_template: false,
+        metadata: Metadata {
+            full_name: "Invert Colors",
+            description: "Invert colors so text and content stand out.",
+            variables: phf_map! {},
+        },
     },
     "vibrance" => BuiltinShader {
         contents: include_str!("shaders/vibrance.glsl.mustache"),
         is_template: true,
+        metadata: Metadata {
+            full_name: "Vibrance",
+            description: "Enhance color saturation.",
+            variables: phf_map! {
+                "balance" => Variable::Dict(phf_map! {
+                    "red" => Variable::Float {
+                        default: 1.0,
+                        min: 0.0,
+                        max: 10.0,
+                    },
+                    "green" => Variable::Float {
+                        default: 1.0,
+                        min: 0.0,
+                        max: 10.0,
+                    },
+                    "blue" => Variable::Float {
+                        default: 1.0,
+                        min: 0.0,
+                        max: 10.0,
+                    },
+                }),
+                "strength" => Variable::Float {
+                    default: 0.15,
+                    min: -1.0,
+                    max: 1.0,
+                },
+            },
+        },
     },
 };
