5abd3f496b9c98cbcbb0558d314f12bd2c09df28
loqusion <38332081+loqusion@users.noreply.github.com>
2024-08-11 22:35:01 -0500

    test(off): use `hyprland_test` attribute

--- tests/it/test_off.rs
+++ tests/it/test_off.rs
@@ -1,7 +1,8 @@
+use proc_macros::hyprland_test;
+
 use crate::common::Space;
 
-#[test]
-#[ignore = "requires hyprland"]
+#[hyprland_test]
 fn smoke() {
     let space = Space::new();
     insta_cmd::assert_cmd_snapshot!(space.hyprshade_cmd().arg("off"), @r###"
