1a4f1e9462d9f147ff1b5d734c73b0d2f6beeb77
loqusion <38332081+loqusion@users.noreply.github.com>
2024-08-12 18:46:30 -0500

    test(auto): snapshot test verifying error message resolving shader

--- tests/it/test_auto.rs
+++ tests/it/test_auto.rs
@@ -21,4 +21,35 @@ mod error {
         [96mSuggestion[0m: For more information, see [URL]
         "###);
     }
+
+    #[test]
+    fn fails_resolving_shader() {
+        let mut space = Space::new();
+        space.with_time("02:00:00").with_config(
+            r#"
+            [[shader]]
+            name = "invalid"
+            default = true
+            "#,
+        );
+        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("auto"), @r###"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+
+        ----- stderr -----
+        Error: 
+           0: [91mresolving shader in config[0m
+           1: [91mshader named "invalid" not found[0m
+
+        Location:
+           [LOCATION]
+
+        Configuration:
+           [33m[HYPRSHADE_CONFIG][39m
+
+        [96mSuggestion[0m: Change the shader name in your configuration, or make sure a shader by that name exists
+        [96mSuggestion[0m: For more information, see [URL]
+        "###);
+    }
 }
