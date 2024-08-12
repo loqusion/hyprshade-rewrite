f5668256af726192c54eb6b06b2ed5e28da8d15f
loqusion <38332081+loqusion@users.noreply.github.com>
2024-08-11 22:34:49 -0500

    feat(proc_macros): `hyprland_test` attribute macro

---
+++ proc_macros/src/lib.rs
@@ -0,0 +1,50 @@
+use std::env;
+
+use lazy_static::lazy_static;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, Attribute, ItemFn,
+};
+
+lazy_static! {
+    static ref IS_HYPRLAND_RUNNING: bool = env::var_os("HYPRLAND_INSTANCE_SIGNATURE").is_some();
+}
+
+#[proc_macro_attribute]
+pub fn hyprland_test(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemFn);
+    // Parsing the attribute is done to validate meta item syntax (in this case, lack thereof)
+    let _ = parse_macro_input!(attr as HyprlandTestAttribute);
+
+    let ignore_attr = if *IS_HYPRLAND_RUNNING {
+        None
+    } else {
+        Some(quote! {
+            #[ignore = "requires hyprland"]
+        })
+    };
+
+    let expanded = quote! {
+        #[test]
+        #ignore_attr
+        #item
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+struct HyprlandTestAttribute {
+    attrs: Vec<Attribute>,
+}
+
+impl Parse for HyprlandTestAttribute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            attrs: input.call(Attribute::parse_outer)?,
+        })
+    }
+}
