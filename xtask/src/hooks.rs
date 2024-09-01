6dd1aef50485871077666d24b36e98d9a4df864d
loqusion <38332081+loqusion@users.noreply.github.com>
2024-08-31 20:32:08 -0500

    build(xtask): better error reporting

--- xtask/src/hooks.rs
+++ xtask/src/hooks.rs
@@ -7,7 +7,7 @@ pub struct RestoreShaderHook {
 impl RestoreShaderHook {
     pub fn new() -> Self {
         let saved_shader = Shader::current().unwrap_or_else(|err| {
-            eprintln!("{err}");
+            report_error(&err);
             None
         });
         eprintln!("Saved shader: {saved_shader:?}");
@@ -18,8 +18,15 @@ impl RestoreShaderHook {
         let Self { saved_shader } = self;
         eprintln!("Restoring shader: {saved_shader:?}");
         match saved_shader {
-            Some(shader) => shader.restore().unwrap_or_else(|err| eprintln!("{err}")),
-            None => Shader::off().unwrap_or_else(|err| eprintln!("{err}")),
+            Some(shader) => shader.restore().unwrap_or_else(|err| report_error(&err)),
+            None => Shader::off().unwrap_or_else(|err| report_error(&err)),
         }
+    }
+}
+
+fn report_error(err: &eyre::Report) {
+    eprintln!("Error:");
+    for cause in err.chain() {
+        eprintln!("  {cause}");
     }
 }
