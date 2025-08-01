e0c7d1603322e4459c93154dc8ec052bf27d0be5
loqusion <loqusion@gmail.com>
2025-07-31 21:01:24 -0500

    test(toggle): add error message tests

--- tests/it/test_toggle.rs
+++ tests/it/test_toggle.rs
@@ -7,6 +7,7 @@ mod error {
     fn empty_arg_fails_without_config() {
         let mut space = Space::new();
         space.with_any_time();
+
         hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle"), @r###"
         success: false
         exit_code: 1
@@ -24,74 +25,8 @@ mod error {
         "###);
     }
 
-    #[test]
-    fn fallback_default_fails_without_config() {
-        let mut space = Space::new();
-        space.with_any_time();
-        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-default"]), @r###"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-
-        ----- stderr -----
-        Error: 
-           0: [91mno configuration file found[0m
-
-        Location:
-           [LOCATION]
-
-        [93mWarning[0m: A configuration file is required to use --fallback-default
-        [96mSuggestion[0m: For more information, see [URL]
-        "###);
-    }
-
-    #[test]
-    fn fallback_auto_fails_without_config() {
-        let mut space = Space::new();
-        space.with_any_time();
-        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-auto"]), @r###"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-
-        ----- stderr -----
-        Error: 
-           0: [91mno configuration file found[0m
-
-        Location:
-           [LOCATION]
-
-        [93mWarning[0m: A configuration file is required to use --fallback-auto
-        [96mSuggestion[0m: For more information, see [URL]
-        "###);
-    }
-
-    #[test]
-    fn fallback_default_fails_without_default_shader() {
-        let mut space = Space::new();
-        space.with_any_time().with_config(r#""#);
-        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-default"]), @r###"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-
-        ----- stderr -----
-        Error: 
-           0: [91mno default shader found in config[0m
-
-        Location:
-           [LOCATION]
-
-        Configuration:
-           [33m[HYPRSHADE_CONFIG][39m
-
-        [96mSuggestion[0m: Make sure a default shader is defined (default = true)
-        [96mSuggestion[0m: For more information, see [URL]
-        "###);
-    }
-
     #[hyprland_test]
-    fn empty_arg_fails_resolving_shader() {
+    fn empty_arg_fails_resolving_default_shader() {
         let mut space = Space::new();
         space.with_any_time().with_config(
             r#"
@@ -125,6 +60,88 @@ mod error {
     }
 
     #[hyprland_test]
+    fn empty_arg_fails_resolving_scheduled_shader() {
+        let mut space = Space::new();
+        space.with_time("01:00:00").with_config(
+            r#"
+                [[shader]]
+                name = "invalid"
+                start_time = 00:00:00
+                end_time = 02:00:00
+            "#,
+        );
+        space.hyprshade_cmd().arg("on").arg("vibrance").run();
+
+        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle"), @r###"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+
+        ----- stderr -----
+        Error: 
+           0: [91mresolving shader in config[0m
+           1: [91mshader named "invalid" not found[0m
+
+        Location:
+           [LOCATION]
+
+        Configuration:
+           [33m[HYPRSHADE_CONFIG][39m
+
+        [96mNote[0m: Since you omitted SHADER from cli arguments, it was inferred from the schedule in your configuration
+        [96mSuggestion[0m: Change the shader name in your configuration, or make sure a shader by that name exists
+        [96mSuggestion[0m: For more information, see [URL]
+        "###);
+    }
+
+    #[test]
+    fn fallback_default_fails_without_config() {
+        let mut space = Space::new();
+        space.with_any_time();
+
+        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-default"]), @r###"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+
+        ----- stderr -----
+        Error: 
+           0: [91mno configuration file found[0m
+
+        Location:
+           [LOCATION]
+
+        [93mWarning[0m: A configuration file is required to use --fallback-default
+        [96mSuggestion[0m: For more information, see [URL]
+        "###);
+    }
+
+    #[test]
+    fn fallback_default_fails_without_default_shader() {
+        let mut space = Space::new();
+        space.with_any_time().with_config(r#""#);
+
+        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-default"]), @r###"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+
+        ----- stderr -----
+        Error: 
+           0: [91mno default shader found in config[0m
+
+        Location:
+           [LOCATION]
+
+        Configuration:
+           [33m[HYPRSHADE_CONFIG][39m
+
+        [96mSuggestion[0m: Make sure a default shader is defined (default = true)
+        [96mSuggestion[0m: For more information, see [URL]
+        "###);
+    }
+
+    #[hyprland_test]
     fn fallback_default_fails_resolving_shader() {
         let mut space = Space::new();
         space.with_any_time().with_config(
@@ -153,6 +170,28 @@ mod error {
            [33m[HYPRSHADE_CONFIG][39m
 
         [96mSuggestion[0m: Change the shader name in your configuration, or make sure a shader by that name exists
+        [96mSuggestion[0m: For more information, see [URL]
+        "###);
+    }
+
+    #[test]
+    fn fallback_auto_fails_without_config() {
+        let mut space = Space::new();
+        space.with_any_time();
+
+        hyprshade_cmd_snapshot!(space.hyprshade_cmd().arg("toggle").args(["vibrance", "--fallback-auto"]), @r###"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+
+        ----- stderr -----
+        Error: 
+           0: [91mno configuration file found[0m
+
+        Location:
+           [LOCATION]
+
+        [93mWarning[0m: A configuration file is required to use --fallback-auto
         [96mSuggestion[0m: For more information, see [URL]
         "###);
     }
